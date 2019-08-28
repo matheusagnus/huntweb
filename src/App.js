ecf42650219dfa13ee4cb26c2bbe808037c675cf
matheusagnus <ferreira.zx@gmail.com>
2019-08-28 15:07:50 -0300

    main page of products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import api from './services/api';
 
-import Header from './components/Header'
-import './styles.css'
+import Header from './components/Header';
+import Main from './pages/main'
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
